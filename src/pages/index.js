b3748dd4d5fbf5c515340494dbedd6a053e01412
Fabi Martinez <104600297+FabiolaMartinez1@users.noreply.github.com>
2024-10-08 14:35:25 -0400

    fix import styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,10 @@
-//theme
-import "primereact/resources/themes/lara-light-cyan/theme.css";   
-//core
-import "primereact/resources/primereact.min.css";  
-//icons
-import "primeicons/primeicons.css";
-//primeflex
-import "primeflex/primeflex.css";
-
-// import Head from "next/head";
-// import Image from "next/image";
-// import localFont from "next/font/local";
-// import styles from "@/styles/Home.module.css";
-// import Login from "@/components/login";
-import Order from "./kitchen/order.js";
-
-// import KitchenSiderBar from "@/components/kitchen/KitchenSidebar";
-// import OrdersPage from "@/kitchen/order.js";
-
-// const geistSans = localFont({
-//   src: "./fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-// const geistMono = localFont({
-//   src: "./fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
+// import Order from "./kitchen/order.js";
+import Login from "./login.js";
 
 export default function Home() {
   return (
     // <Order />
-    // <Login />
-    // <KitchenSiderBar/>
-    <Order />
+    <Login />
   );
 }
 
