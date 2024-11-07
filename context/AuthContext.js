e699f3fdd0c3104f528181ee8e652e6743de497a
Fabi Martinez <104600297+FabiolaMartinez1@users.noreply.github.com>
2024-11-06 21:05:54 -0400

    localStorage

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -1,5 +1,4 @@
 import { createContext, useContext, useEffect, useState } from 'react';
-import { setCookie, getCookie, removeCookie } from 'cookies-next';
 import AuthService from '@/service/AuthService';
 
 const AuthContext = createContext();
@@ -10,8 +9,9 @@ const AuthProvider = ({ children }) => {
     const authService = new AuthService();
 
     useEffect(() => {
-        // Intenta obtener el token almacenado en las cookies al cargar el componente
-        const token = getCookie('authToken');
+        // Intenta obtener el token almacenado en localStorage al cargar el componente
+        const token = localStorage.getItem('authToken');
+        console.log("authtoken", token)
         if (token) {
             setAuthToken(token);
             setIsAuthenticated(true);
@@ -21,14 +21,9 @@ const AuthProvider = ({ children }) => {
     const login = async (email, password) => {
         try {
             const response = await authService.login(email, password);
-            console.log("response",response);
-            setCookie('authToken', response.access_token, { httpOnly: true, secure: false });
-            console.log("authToken",response.access_token);
-            setAuthToken(response.access_token); 
-            console.log("setAuthToken",response.access_token);
+            localStorage.setItem('authToken', response.access_token); // Guardar en localStorage
+            setAuthToken(response.access_token);
             setIsAuthenticated(true);
-
-            console.log("get",getCookie())
             return response;
         } catch (error) {
             console.error('Login failed:', error);
@@ -37,8 +32,8 @@ const AuthProvider = ({ children }) => {
     };
 
     const logout = () => {
-        removeCookie('authToken');
-        setAuthToken(null); 
+        localStorage.removeItem('authToken'); // Limpiar localStorage
+        setAuthToken(null);
         setIsAuthenticated(false);
     };
 
@@ -51,42 +46,3 @@ const AuthProvider = ({ children }) => {
 
 export const useAuth = () => useContext(AuthContext);
 export { AuthContext, AuthProvider };
-//---------------------------------
-
-// import AuthService from '@/service/AuthService';
-
-// import React, { createContext, useState } from 'react';
-
-// const AuthContext = createContext();
-
-// const AuthProvider = ({ children }) => {
-//     const [authToken, setAuthToken] = useState(null); // TODO: Maybe use cookies instead
-//     const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-//     const authService = new AuthService();
-
-//     const login = async (email, password) => {
-//         //console.log('Context::Logging in with:', email);
-//         try {
-//             const response = await authService.login(email, password);
-//             setAuthToken(response.access_token);
-//             await setIsAuthenticated(true);
-//             return response;
-//         } catch (error) {
-//             console.error('Login failed:', error);
-//         }
-//     };
-
-//     const logout = () => {
-//         setAuthToken(null);
-//         setIsAuthenticated(false);
-//     };
-
-//     return (
-//         <AuthContext.Provider value={{ authToken, isAuthenticated, login, logout }}>
-//             {children}
-//         </AuthContext.Provider>
-//     );
-// };
-
-// export { AuthContext, AuthProvider };
