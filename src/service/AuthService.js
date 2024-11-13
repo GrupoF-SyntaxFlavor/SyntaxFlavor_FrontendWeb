d63b5a267b1c56c29e8cec8cf42fe3868a1a8292
Alejandro Carrasco <carrascoalejandro2003@gmail.com>
2024-11-12 22:17:11 -0400

    aaaa

--- src/service/AuthService.js
+++ src/service/AuthService.js
@@ -5,6 +5,8 @@ export default class AuthService {
 
     //Método de login
     async login(email, password){
+        console.log("has value in envs of: ", process.env.NEXT_PUBLIC_BACKEND_URL); // erase later
+        console.log("has value in envs of: ", process.env.NEXT_PUBLIC_MINIO_URL);
         try{
             //console.log("Logging in with email and password", email, password); // erase later
             console.log("sending to", `${this.BASE_URL}/api/v1/public/login`); // erase later
