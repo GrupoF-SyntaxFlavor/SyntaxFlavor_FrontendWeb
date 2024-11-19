5ee5384b5d07c9dd0954bb7687c5cd5a663dd7d1
Alejandro Carrasco <carrascoalejandro2003@gmail.com>
2024-11-19 17:39:51 -0400

    all endpoints consumed not tested

--- src/service/ReportService.js
+++ src/service/ReportService.js
@@ -60,4 +60,42 @@ export default class ReportService {
             throw error;
         }
     }
+
+    /**
+     * Fetches the monthly sales report for the last 6 months.
+     * @param {string} token - The JWT token for authorization.
+     * @param {string} startDate - The start date for the report.
+     * @param {string} endDate - The end date for the report.
+     * @param {string} top - The number of top products to include in the report.
+     * @returns {Promise<Object>} - The sales report data.
+     * @throws {Error} - If an error occurs while fetching the report.
+     * @async
+     */
+    async getMostSoldItems(
+        token,
+        startDate = get30DaysAgoDate(),
+        endDate = new Date(),
+        top = 10
+    ) {
+        try {
+            const response = await fetch(`${this.BASE_URL}/api/v1/report/menu/most-sold?startDate=${startDate}&endDate=${endDate}&top=${top}`, {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                    'Content-Type': 'application/json',
+                }
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            const data = await response.json();
+            console.log("Most Sold Items Data:", data.payload);
+            return data.payload; // Return the most sold items report
+        } catch (error) {
+            console.error("Error fetching most sold items report:", error);
+            throw error;
+        }
+    }
 }
