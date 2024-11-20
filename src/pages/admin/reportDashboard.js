07bef37f729819714e46ce693f6966fe655b6e7f
dianamongar <dianaaneliz@gmail.com>
2024-11-20 18:45:49 -0400

    cambios pie chart

--- src/pages/admin/reportDashboard.js
+++ src/pages/admin/reportDashboard.js
@@ -8,8 +8,12 @@ import { AuthContext } from "../../../context/AuthContext";
 
 const ReportDashboard = () => {
     const [ordersKPI, setOrdersKPI] = useState({});
+    const [orderKPIChartOptions, setOrderKPIChartOptions] = useState({});
     const [weeklySales, setWeeklySales] = useState({});
     const [mostSoldItem, setMostSoldItem] = useState({});
+    const [totalOrders, setTotalOrders] = useState({});
+    const [acceptedOrders, setAcceptedOrders] = useState({});
+    const [cancelledOrders, setCancelledOrders] = useState({});
 
     const { authToken } = useContext(AuthContext);
 
@@ -19,6 +23,52 @@ const ReportDashboard = () => {
         reportService.getOrdersKPI(authToken)
             .then(data => {
                 setOrdersKPI(data);
+                console.log(ordersKPI);
+                setAcceptedOrders(ordersKPI.totalAcceptedOrders);
+                setCancelledOrders(ordersKPI.totalCancelledOrders);
+                setTotalOrders(ordersKPI.totalOrders);
+
+                setOrderKPIChartOptions({
+                    chart: {
+                        plotBackgroundColor: null,
+                        plotBorderWidth: null,
+                        plotShadow: false,
+                        type: 'pie'
+                    },
+                    title: {
+                        text: 'Reporte de Órdenes Aceptadas y Canceladas',
+                        align: 'center'
+                    },
+                    tooltip: {
+                        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+                    },
+                    accessibility: {
+                        point: {
+                            valueSuffix: '%'
+                        }
+                    },
+                    plotOptions: {
+                        pie: {
+                            allowPointSelect: true,
+                            cursor: 'pointer',
+                            borderRadius: 5,
+                            dataLabels: {
+                                enabled: true,
+                                format: '<b>{point.name}</b>: {point.y} ({point.percentage:.1f}%)',
+                                distance: -50
+                            }
+                        }
+                    },
+                    series: [
+                        {
+                            name: "Porcentaje de órdenes",
+                            data: [
+                                { name: "Aceptadas", y: acceptedOrders },
+                                { name: "Canceladas", y: cancelledOrders }
+                            ],
+                        }
+                    ]
+                });
             })
             .catch(error => {
                 console.error("Failed to fetch orders KPI:", error);
@@ -97,9 +147,16 @@ const ReportDashboard = () => {
                     </div>
                 </div>
                 <div className="column">
-                    <h2>Completed Orders</h2>
-                    <div className="data-placeholder">Grafico de torta</div>
+                    <h2>Órdenes</h2>
                     <div className="data-placeholder">
+                        { Object.keys(ordersKPI).length > 0 ? (
+                            <HighchartsReact
+                                highcharts={Highcharts}
+                                options={orderKPIChartOptions}
+                            />
+                        ) : (
+                            <div>Cargando datos...</div>
+                        )}
                         {JSON.stringify(ordersKPI)}
                     </div>
                 </div>
